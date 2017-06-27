3b054402e2b3fcfb954aee2c5116a20c31f0b6f8
artpylon <matthewgray88@gmail.com>
2017-06-27 12:46:17 -0400

    Added file/events file with file events Also, added script to run folder index event function on page load in index.js

---
+++ assets/scripts/file/events.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const getFormFields = require(`../../lib/get-form-fields`)
+
+const fileapi = require('./api')
+const fileui = require('./ui')
+const store = require('./store')
+
+// File events
+const onIndexFolder = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  fileapi.indexFolder(data)
+    .then(fileui.indexFolderSuccess)
+    .catch(fileui.indexFolderFailure)
+}
+
+const onIndexFile = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  fileapi.indexFolder(data)
+    .then(fileui.indexFileSuccess)
+    .catch(fileui.indexFileFailure)
+}
+
+const onAddFile = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  fileapi.addFile(data)
+    .then(fileui.addFileSuccess)
+    .catch(fileui.addFileFailure)
+}
+
+const onUpdateFile = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  fileapi.updateFile(data)
+    .then(fileui.updateFileSuccess)
+    .catch(fileui.updateFileFailure)
+}
+
+const onDeleteFile = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  fileapi.deleteFile(data)
+    .then(fileui.deleteFileSuccess)
+    .catch(fileui.deleteFileFailure)
+}
+
+const addHandlers = () => {
+}
+
+module.exports = {
+  addHandlers,
+  onIndexFolder
+}
