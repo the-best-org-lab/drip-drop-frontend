1c44706b9a6ba7b0008b298e474ff0a4eafa9450
artpylon <matthewgray88@gmail.com>
2017-06-27 11:22:56 -0400

    Added api calls to scripts/files/api.js

---
+++ assets/scripts/file/api.js
@@ -0,0 +1,55 @@
+'use strict'
+const config = require('../config')
+const store = require('../store')
+
+// FILE ACTIONS
+const fileIndex = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/files',
+    method: 'GET',
+    headers: {
+      Authorization: 'Token token=' + store.userToken
+    }
+  })
+}
+
+const addFile = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/files',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.userToken
+    },
+    data
+  })
+}
+
+const updateFile = function (data) {
+  return $.ajax({
+  // fileID to be stored locally when update modal is launched
+    url: config.apiOrigin + '/files/' + store.fileId,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.userToken
+    },
+    data
+  })
+}
+
+const deleteFile = function () {
+  return $.ajax({
+    // fileID to be stored locally when delete modal is launched
+    url: config.apiOrigin + '/files/' + store.fileId,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.userToken
+    }
+  })
+}
+
+module.exports = {
+  fileIndex,
+  addFile,
+  updateFile,
+  deleteFile
+}
