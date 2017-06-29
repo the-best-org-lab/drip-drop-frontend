edeb9b21c4a0b065c2fdecf21e91fe4d92123491
artpylon <matthewgray88@gmail.com>
2017-06-28 21:36:17 -0400

    update api origin in config js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,7 @@
 const config = {
   apiOrigins: {
     development: 'http://localhost:4741',
-    production: 'https://brewscout.herokuapp.com/'
+    production: 'https://murmuring-plains-65734.herokuapp.com'
   }
 }
 
